bdaad9c40a55d8cb6d4b99132cb688c2e3f501d1
kerat6 <t.ahmadieh@outlook.com>
2024-08-22 10:50:17 -0400

    Add compChoice function that choose rps.

--- javascript.js
+++ javascript.js
@@ -5,3 +5,15 @@ console.log("Hello World");
 // assign rock paper and scissors to each of these numbers as string value
 
 
+function getComputerChoice() {
+
+    let compChoice = Math.floor(Math.random() * 3);
+
+    if (compChoice === 0) {
+        console.log("rock");
+    }
+    else if (compChoice === 1) { console.log("paper"); }
+    else console.log("scissors");
+}
+
+getComputerChoice();
